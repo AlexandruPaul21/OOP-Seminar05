f216040b2529ad1bafae06b13d11a89c2772597d
alex <alexandru.sirbu2104@gmail.com>
2022-04-19 16:29:03 +0300

    Final version

--- beverage.h
+++ beverage.h
@@ -7,21 +7,40 @@
 
 #include <string>
 #include <iostream>
+#include <sstream>
 
 using std::string;
 
 class Beverage {
     string description;
+    int whatever;
 public:
-    Beverage(const string &desc) : description{desc} {};
-    Beverage()=default;
+    Beverage(const string &desc) : description{desc} { this->whatever = 123210; };
+
+    Beverage() = default;
 
     virtual double price() { return 150; }
 
     void print() {
-        std::cout << description<<" for $"<<std::to_string(this->price());
+        std::cout << description << " for $" << std::to_string(this->price()) << " " << whatever;
     }
 
+    friend std::ostream &operator<<(std::ostream &s, const Beverage &b) {
+        s << b.description << ";" << b.whatever;
+        return s;
+    }
+
+    friend std::istream &operator>>(std::istream &is, Beverage &b) {
+        std::string s;
+        std::string s1;
+
+        std::getline(is, s, ';');
+        std::getline(is, s1, ';');
+
+        b.description = s;
+        b.whatever = std::stoi(s1);
+        return is;
+    }
 };
 
 
